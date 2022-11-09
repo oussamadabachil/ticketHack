e7cabb559167b47ad7795485418ec4236e39aed9
Your name <Your email>
2022-11-09 16:04:10 +0100

    ijdj

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -2,44 +2,36 @@ var express = require('express');
 var router = express.Router();
 require('../models/connexion')
 const trips = require('../models/trips')
-const idOrder = require('./cart')
-
-
-
-
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 let arrayBook = []
 
-router.post('/idTrip', (req, res)=> {
+router.post('/idTrip', (req, res) => {
 
-//faire une boucle pour verifier si l'id est deja dans le tableau
-  if(arrayBook.includes(req.body.idTrip)){
+  //faire une boucle pour verifier si l'id est deja dans le tableau
+  if (arrayBook.includes(req.body.idTrip)) {
     res.json("you already booked this trip")
   }
-  else{
+  else {
     res.json("you booked this trip")
     arrayBook.push(req.body.idTrip)
     console.log(arrayBook)
   }
-  
+
+})
+
+router.get('/idTrip', (req, res) => {
+
+  for (let i = 0; i < idOrder.length; i++) {
 
 
-    
- })
-
-router.get('/idTrip', (req, res)=> {
-
-  for(let i = 0;i<idOrder.length;i++){
-
-   
-    trips.findById(idOrder[0], (err, data)=>{
-      if(err){
+    trips.findById(idOrder[0], (err, data) => {
+      if (err) {
         res.json(err)
-      }else{
+      } else {
         res.json(data)
         idOrder.shift()
 
--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -24,15 +24,6 @@ router.get("/getTrip/:departure/:arrival/:date",(req,res)=>{
     let date = req.params.date  
     
 
-    // let newDate = moment(date).format("YYYY-MM-DD")
-
-
-   
-    //let newDate = new Date(date).toLocaleDateString("en")
-    //console.log(newDate)
-
-
-
 
     
     trips.find({
@@ -42,55 +33,15 @@ router.get("/getTrip/:departure/:arrival/:date",(req,res)=>{
     }).then(data=>{
       
        
-        //display only the date of the trip
-       
-        
         if(data.length>0){
-           // if(new Date(data.date).getTime()>=dateOfTheDay){
-
-
-        //      let dateOfTheTrip = new Date(data.date).getTime()
-        // if(dateOfTheTrip>dateOfTheDay){
+       
             res.json({result:true,data})
         
         }
-    //}
+
     else{
             res.json({result:false,message:"No trip found"})
         }
     })
 })
-
-    
-//     let departure = req.params.departure
-//     let arrival = req.params.arrival
-//     let date = req.params.date
-
-
-//     console.log(departure,arrival,date)
-//     let newDate = new Date(date).toLocaleDateString("en")
-//    trips.find({departure:departure,arrival:arrival,date:newDate}).then(data=>{
-//             console.log(data)
-//             res.json(data)
-
-//             console.log(data)
-//         })
-    
-    // let departure = req.params.departure
-    // let arrival = req.params.arrival
-    // let timestamp = req.params.timestamp
-    // let date = timestamp.split("T")
-    // let dateInput = date[0]
-    // let dateInputNew = new Date(dateInput).getTime()
-
-    // trips.find({departure:departure,arrival:arrival,timestamp:dateInputNew}).then(data=>{
-    //     console.log(data)
-    //     res.json(data)
-    // })
-    
-
-
-
-/* GET home page. */
-
 module.exports = router;
