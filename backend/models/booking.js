0db55767590ee647c370328ceb11250fb821d982
OussamaDabachil <oussamadabachilpro@gmail.com>
2022-11-09 16:00:05 +0100

    kjdkdjkj

---
+++ backend/models/booking.js
@@ -0,0 +1,22 @@
+// const mongoose = require('mongoose');
+
+// const Schema = mongoose.Schema;
+
+// const bookingSchema = new Schema({
+    
+//    departure:String,
+//    arrival:String,
+//    date:Date,
+//    price:Number,
+
+//      });
+
+     
+// const Trip = mongoose.model('trips', tripsSchema);
+// module.exports = Trip;
+
+
+
+
+
+      
